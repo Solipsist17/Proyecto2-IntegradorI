9f192350cb8a224ba41f43612140f5c781c61236
Gogoz22 <gogozgallardo22@gmail.com>
2023-06-23 05:20:47 -0500

    Adicion de divs de acerca de nosotros ,ubicación y redes sociales.Arreglo de estilos de mi cuenta

--- views/cuenta/index.php
+++ views/cuenta/index.php
@@ -6,6 +6,8 @@
      <link rel="stylesheet" href="<?php echo constant('URL')?>public/css/default.css">
     <link rel="stylesheet" href="<?= constant('URL') ?>public/css/header.css">
     <link rel="stylesheet" href="<?= constant('URL') ?>public/css/footer.css">
+    <link rel="stylesheet" href="<?= constant('URL') ?>public/css/cuenta.css">
+    
     <!-- <link rel="stylesheet" href="<?= constant('URL') ?>public/css/estilo_registro.css"> -->
 
   </head>
@@ -15,39 +17,82 @@
     <h1>Mi cuenta</h1>
 
     <form action="<?php echo constant('URL'); ?>usuario/modificarUsuario" method="POST">
+
+      <ul>
+    
+      <li>
         <label for="username">Username</label>
+        <ul>
         <input id="username" name="username" type="text" placeholder="Ingrese el Nombre de Usuario" value="<?= $this->usuario->username ?>" required>
+        </ul>
+      </li>
 
+      <li>
         <label for="password">Password</label>
+        <ul>
         <input id="password" name="password" type="password" placeholder="Enter Password" value="<?= $this->usuario->password ?>" required>
+        </ul>
+      </li>
 
+      <li>
         <label for="correo">Correo</label>
+        <ul>
         <input id="correo" name="correo" type="text" placeholder="Ingrese su correo" value="<?= $this->usuario->correo ?>" required>
+        </ul>
+      </li> 
+      
 
+      <li>
         <label for="nombre">Nombre</label>
+        <ul>
         <input id="nombre" name="nombre" type="text" placeholder="Ingrese su nombre" value="<?= $this->usuario->nombre ?>" required>
+        </ul>
+      </li>
 
+
+      <li>
         <label for="apellido">Apellido</label>
+        <ul>
         <input id="apellido" name="apellido" type="text" placeholder="Ingrese su apellido" value="<?= $this->usuario->apellido ?>" required>
-
+        </ul>
+      </li>
+        
+      <li>
         <label for="dni">DNI</label>
+        <ul>
         <input id="dni" name="dni" type="text" placeholder="Ingrese su DNI" value="<?= $this->usuario->dni ?>" >
+        </ul>
+      </li>
 
+      <li>
         <label for="telefono">Teléfono</label>
+        <ul>
         <input id="telefono" name="telefono" type="text" placeholder="Ingrese su teléfono" value="<?= $this->usuario->telefono ?>" >
+        </ul>
+      </li>
+    </ul>
+        
+    <ul>
+      <div class="modifier">
+      <input class="modificar" type="submit" value="Modificar">
+      </div>
+    </ul>
 
-        <input type="submit" value="Modificar">
     </form>
 
     <form action="<?php echo constant('URL'); ?>usuario/cerrarSesion" method="POST">
-        <label for="logout">Cerrar sesión</label>
-        <input id="logout" type="submit" value="Cerrar sesión">
+      <div class="cerrar" id="Cerrar">
+        <label for="logout">Cerrar sesión :</label>
+        <input class="close" id="logout" type="submit" value="Cerrar sesión">
+      </div>
+
+
     </form>
 
     <!-- <?php var_dump($this->usuario) ?> -->
     <?= $this->mensaje ?>
 
-     <?php require "views/footer.php"; ?>
+    <?php require "views/footer.php"; ?>
 
   </body>
 
--- views/main/index.php
+++ views/main/index.php
@@ -6,7 +6,10 @@
     <link rel="stylesheet" href="<?php echo constant('URL')?>public/css/default.css">
     <link rel="stylesheet" href="<?= constant('URL') ?>public/css/header.css">
     <link rel="stylesheet" href="<?= constant('URL') ?>public/css/estilo_slider.css">
+    <link rel="stylesheet" href="<?= constant('URL') ?>public/css/tarjeta.css">
+    <link rel="stylesheet" href="<?= constant('URL') ?>public/css/tarjeta2.css">
     <link rel="stylesheet" href="<?= constant('URL') ?>public/css/footer.css">
+
     
 
     <title>Inicio</title>
@@ -26,7 +29,83 @@
 
     <script src="public/js/js_slider.js"></script>
 
-    <h1>Esta es la vista de inicio</h1>
+    <h1 class="infowe">Acerca de nosotros</h1>
+
+    <div class="contenedor">
+        <div class="target">
+            <div class="sign">
+                <img src="public/img/logo.png">
+
+            </div>
+            <p>
+                "En Polotex, nuestro principal objetivo es brindar a nuestros clientes una experiencia de compra excepcional y satisfacer sus necesidades de vestimenta con productos de calidad. Nuestra empresa se enorgullece de ofrecer prendas modernas, cómodas y duraderas que se adaptan a diferentes estilos y ocasiones."
+            </p>
+            </div>
+
+        <div class="target-2">
+            <img src="public/img/info2.png">
+        </div>
+        
+    </div>
+
+    <!----------------->
+
+    <div class="container-texto">
+      <text class ="head-text"><h1>Encuentras nuestras tienda :</h1></text>    
+    </div>
+
+    <div class="container-mapa">
+    <div class="container-texto"><text>#Piura</text></div>    
+    
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3973.434518786919!2d-80.63110892429698!3d-5.19418169478338!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x904a107ec77a24a5%3A0xc8cc5adb847d2f31!2sPolotex!5e0!3m2!1ses-419!2spe!4v1687503516139!5m2!1ses-419!2spe" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+
+    <div class="container-texto"><text>#Tumbes</text></div> 
+
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3982.080446767582!2d-80.45980192406608!3d-3.56896344238387!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9033f2d4fb886923%3A0x2321837c625a663c!2sPOLOTEX!5e0!3m2!1ses-419!2spe!4v1687507567132!5m2!1ses-419!2spe" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+
+    </div>
+
+    <div class="contenedor2">
+
+        <div class="video-container">
+            <video muted autoplay loop><source src="public/vid/polotex.mp4" type="video/mp4"></video> 
+        </div>
+
+
+    
+        <h1 class="head-target2">Enlaces a Plataformas</h1>
+
+        <div class="redes">
+        <img src="public/img/facebook.png" usemap="#facebook">
+            <map name="#facebook">
+            <area target="_blank" alt="" title="" href="https://www.facebook.com/polotex" coords="3,0,597,599" shape="rect">
+            </map>
+
+        <img src="public/img/imstagram.png" usemap="#imstagram">
+            <map name="#imstagram">
+            <area target="_blank" alt="" title="" href="https://www.instagram.com/polotex_plt/?hl=es" coords="1,8,1024,1022" shape="rect">
+            </map>
+
+        <img src="public/img/tiktok.png" usemap="#tiktoks">
+            <map name="#tiktoks">
+            <area target="_blank" alt="" title="" href="https://www.tiktok.com/@polotex1" coords="1,8,1024,1022" shape="rect">
+            </map>
+
+        <img src="public/img/whatsapp.png" usemap="#whatsapp">
+            <map name="#whatsapp">
+            <area target="_blank" alt="" title="" href="https://wa.link/c1mjag" coords="1,8,1024,1022" shape="rect">
+            </map>
+        </div>
+
+
+      </td>
+        </div>
+
+                
+    
+   
+    </div>
+
 
     <?php require "views/footer.php"; ?>
 </body>
