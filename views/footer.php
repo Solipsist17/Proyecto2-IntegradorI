ecb406c22dcdc72d4b6572685f6d5aa61ab63d54
KG0112 <karigiane2000@hotmail.com>
2023-06-23 00:12:48 -0500

    Mejora en el footer

--- views/footer.php
+++ views/footer.php
@@ -1,3 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Footer</title>
+	<link rel="stylesheet" type="text/css" href="public/css/footer.css">
+</head>
 
 <body>
 <footer class="footer">
@@ -8,26 +16,29 @@
 						<img src="<?php echo constant('URL')?>public/img/Logo.png" alt="Logo-polotex">
 					</a>
 				</figure> 
-			</div>
+		    </div>
 
 
 			<div class="box">
 				<h2>Ayuda sobre la cuenta</h2>
-				<p>Inicio de Sesión</p>
-				<p>Ayuda sobre perfil</p>
-				<p>Ayuda sobre la configuración</p>
-				<p>Seguridad</p>
+				<a href="#"><p>Inicio de Sesión</p></a>
+				<a href="#"><p>Ayuda sobre perfil</p></a>
+				<a href="#"><p>Ayuda sobre la configuración</p></a>
+				<a href="#"><p>Seguridad</p></a>
 			</div>
 				
 
 			<div class="box">
 				<h2>Ayuda sobre pagos</h2>
-				<p>Gestion de pagos</p>
-				<p>Politicas de reembolso</p>
-				<p>Formas de pago</p>
-				<p>Ayuda sobre cobros</p>
+				<a href="#"><p>Gestion de pagos</p></a>
+				<a href="#"><p>Politicas de cambio y devoluciones</p></a>
+				<a href="#"><p>Formas de pago</p></a>
+				<a href="#"><p>Ayuda sobre cobros</p></a>
 			</div>
+		</div>
 
 		</div>
+
 	</footer>
 </body>
+</html>
